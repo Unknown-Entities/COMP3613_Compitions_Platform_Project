8158480255aa57c4068381714f2b9eb7184734ab
GillianoA <agard.gilliano@gmail.com>
2023-10-22 14:03:09 -0400

    Competition model update

--- App/models/competition.py
+++ App/models/competition.py
@@ -2,7 +2,7 @@ from App.database import db
 
 class Competition(db.Model):
     competitionID = db.Column(db.Integer, primary_key=True)
-    studentID = db.Column(db.Integer, db.ForeignKey("student.id"))
+    winnerID = db.Column(db.Integer, db.ForeignKey("student.id"))
     compName = db.Column(db.String(120), nullable=False)
     prize = db.Column(db.String(120), nullable=False)
     region = db.Column(db.String(120), nullable=False)
@@ -15,6 +15,8 @@ def __init__(self, compName, prize, region, maxEntrants):
     self.region = region
     self.maxEntrants = maxEntrants
 
+def addWinner(self, winnerID):
+    self.winnerID = winnerID
 
 def __repr__(self):
     return f'<Competition {self.competitionID} - {self.compName}>'
