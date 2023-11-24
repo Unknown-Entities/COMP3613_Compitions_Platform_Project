4cb87a6e3526f94a49a50de465268ee953642046
Aadil Ali <60389946+Aadil-speck@users.noreply.github.com>
2023-11-24 03:54:19 +0000

    minor fix to notification module

--- App/models/notification.py
+++ App/models/notification.py
@@ -1,21 +1,24 @@
 from App.database import db
-# from datetime import datetime 
+from datetime import datetime 
 
 class Notification(db.Model):
     notificationID = db.Column(db.Integer, primary_key=True)
     studentID = db.Column(db.Integer,  db.ForeignKey('student.id'))
+    student_email = db.Column(db.String, db.ForeignKey('user.email'))
     message = db.Column(db.String(255), nullable=False)
-   # timestamp = db.Column(db.DateTime, default=datetime.now())
+    timestamp = db.Column(db.DateTime, default=datetime.now())
 
-def __init__(self, studentID, message):
+def __init__(self, studentID, message, student_email):
     self.studentID = studentID
     self.message = message
+    self.student_email = student_email
 
     def toJSON(self):
         return {
             'notificationID' : self.notificationID,
             'studentID' : self.studentID,
             'message' : self.message,
+            'student_email': self.student_email,
             'timestamp': self.payment_date.strftime("%Y/%m/%d, %H:%M:%S")
         }
     
