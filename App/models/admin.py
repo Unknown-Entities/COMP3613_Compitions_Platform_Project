8e0278394802600873bfa88660dba67aa5878495
Aadil Ali <60389946+Aadil-speck@users.noreply.github.com>
2023-11-24 03:48:33 +0000

    creation of admin module

---
+++ App/models/admin.py
@@ -0,0 +1,26 @@
+from .user import User
+from App.database import db 
+
+class Student(User):
+    __tablename__ = 'admin'
+    id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    profile = db.relationship('Profile', backref=db.backref('admin', lazy='joined'))
+    competition = db.relationship('Competition', backref=db.backref('competitor', lazy='joined'))
+
+    def __init__(self, username, password, email):
+        self.username = username
+        self.set_password(password)
+        self.user_type = "admin"
+        self.email = email
+
+
+    def __repr__(self):
+        return f'<Admin {self.id} {self.username} {self.email}>'
+    
+    def toJSON(self):
+        return{
+            'id': self.id,
+            'username': self.username,
+            'type': 'admin',
+            'email': self.email
+        }
