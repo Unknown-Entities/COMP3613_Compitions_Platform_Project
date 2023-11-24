700652dee58f57ec57bfd0aea4d0a6e81e104d1a
Aadil Ali <60389946+Aadil-speck@users.noreply.github.com>
2023-11-24 03:41:40 +0000

    edit to user model structure

--- App/models/user.py
+++ App/models/user.py
@@ -8,15 +8,19 @@ class User(db.Model, UserMixin):
     username =  db.Column(db.String, nullable=False, unique=True)
     user_type = db.Column(db.String, nullable=False)
     password = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(200), nullable=False)
 
-    def __init__(self, username, password):
+
+    def __init__(self, username, password, email):
         self.username = username
-        self.set_password(password)
+        self.email = email
+        self.set_password(password)        
 
     def get_json(self):
         return{
             'id': self.id,
-            'username': self.username
+            'username': self.username,
+            "email": self.email
         }
 
     def set_password(self, password):
@@ -28,4 +32,5 @@ class User(db.Model, UserMixin):
         return check_password_hash(self.password, password)
 
     def __repr__(self):
-        return f'<User {self.id} {self.username}'
+        return f'<User {self.id} {self.username} {self.email}'
+    
